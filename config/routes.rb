ce98d19bd7453ab0f1b4e80d0c1ececa770e0f60
frankda <suyunyi@gmail.com>
2020-03-14 15:49:32 +1100

    user module finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, :only => [:new, :create, :index]
+  resources :users, :only => [:sing, :create, :index]
+
+  get '/users/login' => 'session#new'
+  post '/users/login' => 'session#create'
+  delete '/users/login' => 'session#delete'
+
+  root :to => 'pages#home'
 end
