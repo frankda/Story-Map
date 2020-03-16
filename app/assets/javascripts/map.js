d304ccc7b0f2b5d0994a72ec9de12d2464384685
frankda <suyunyi@gmail.com>
2020-03-16 17:49:19 +1100

    map update

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
+// Init map
 if ($('#mapid').length === 0) return;
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
@@ -11,6 +11,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     tileSize: 512,
     zoomOffset: -1
 }).addTo(mymap);
+// ----------------------------
 
 L.marker([51.5, -0.09]).addTo(mymap)
     .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
@@ -20,13 +21,13 @@ var popup = L.popup()
 .setContent("I am a standalone popup.")
 .openOn(mymap);
 
-var popup = L.popup();
+var popup = L.popup()
 
 function onMapClick(e) {
     popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(mymap);
+    .setLatLng(e.latlng)
+    .setContent("I am a standalone popup.")
+    .openOn(mymap);
 }
 
 mymap.on('click', onMapClick);
